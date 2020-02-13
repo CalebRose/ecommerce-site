5c4326daef3d104836085015f85edb64d5003eec
CalebRose <ctrose17@gmail.com>
2020-02-12 20:53:55 -0600

    Updating app; shop components added; will add sweaters later

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import React from "react";
 import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
 import HomePage from "./components/homepage/homepage.component";
-
-const Argyle = () => (
-  <div>
-    <h1>ARGYLE</h1>
-  </div>
-);
+import ShopPage from "./components/shop/shop.component";
+import "./App.css";
 
 function App() {
   return (
@@ -14,7 +10,7 @@ function App() {
       <Router>
         <Switch>
           <Route exact path="/" component={HomePage} />
-          <Route path="/argyle" component={Argyle} />
+          <Route path="/shop" component={ShopPage} />
           {/* <Route exact path="/topics" component={TopicsList} />
         <Route path="/topics/:topicId" component={TopicDetail} /> */}
         </Switch>

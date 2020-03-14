f894bb28e6eab60e71dbc70c05454fd16a0cf4a0
CalebRose <ctrose17@gmail.com>
2020-03-14 16:16:50 -0500

    Multiple items to cart; new shop data

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -2,3 +2,8 @@ import CartActionTypes from "./cart.types";
 export const ToggleCart = () => ({
   type: CartActionTypes.TOGGLE_CART_HIDDEN
 });
+
+export const addItem = item => ({
+  type: CartActionTypes.ADD_ITEM,
+  payload: item
+});
